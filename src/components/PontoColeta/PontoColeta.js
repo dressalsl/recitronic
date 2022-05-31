90755021d43b54c028004317b4f8f7793686565f
lhaislla <lhaislla@gmail.com>
2022-05-31 17:28:20 -0300

    Descriçaõ pontos de coleta

--- src/components/PontoColeta/PontoColeta.js
+++ src/components/PontoColeta/PontoColeta.js
@@ -18,24 +18,54 @@ function PontoColeta() {
             <div >
                 <input type="text" className={styles.search} placeholder="Faça uma busca pelo ponto de coleta mais próximo de você!"/>
                 <img src={Pesquisa} className={styles.buttonFilter} alt="Buscar"/>
+
             </div>
             <div className={styles.pontoColeta}>
                 <h1 className={styles.tituloPontoColeta}>Pontos de Coleta</h1>
+
             </div>
             <div className={styles.cardPontos}>
                 <CardContent className={styles.card}>
                      <img src={Reecicle} alt=""/>
+                     <Typography className ={styles.text}>REECICLE-iPUTINGA</Typography>
+                        
+                       
+                            
                 </CardContent>  
-                <Typography>REECICLE-iPUTINGA</Typography>
+                <ul className ={styles.desc}>
+                    <p>CPF/CNPJ: 25.257.840/0001-65</p> 
+                    <p>Rua: Rua Rocha Pombo</p> 
+                    <p>Email:contato@reeecicle.com </p>
+                </ul>
+               
+
+
+               
+                
                  {/* <svg data-testid="StarBorderIcon"></svg> */}
                  <CardContent className={styles.card}>
                      <img src={Fundaj} alt=""/>
+                     <Typography className ={styles.text}>Fundaj</Typography>
                 </CardContent>
-                <Typography>Fundaj</Typography>
+                <ul className ={styles.desc}>
+                            <p>CPF/CNPJ:09.773.169/0001-59 </p> 
+                            <p>Rua:R. Henrique Dias </p> 
+                            <p>Email:contato@fundaj.com </p>
+                </ul>
+                    
+               
+               
                 <CardContent className={styles.card}>
                      <img src={EcoRecife} alt=""/>
+                     <Typography className ={styles.text}>Eco Recife</Typography> 
+                     
                 </CardContent>
-                <Typography>Eco Recife</Typography>        
+                <ul className ={styles.desc}>
+                            <p>CPF/CNPJ:23.688.897/0001-93</p> 
+                            <p>Rua:República da Argélia com a Rua Cristiano</p> 
+                            <p>Email:EcoRecife@recife.com</p>
+                </ul>
+                
             </div>
             <Footer />
         </>
